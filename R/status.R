14d47c76d5df5110cbab72be26b6af3cd6f4666b
Jeroen Ooms <jeroenooms@gmail.com>
2020-11-22 00:44:38 +0100

    Strip the .git postfix

--- R/status.R
+++ R/status.R
@@ -11,6 +11,7 @@
 #' @param deployed_packages string with deployed artifacts
 gh_app_set_commit_status <- function(repo, sha, url, universe, deployed_packages){
   repo <- sub("https?://github.com/", "", repo)
+  repo <- sub("\\.git$", "", repo)
   token <- gh::gh_app_token(repo)
   endpoint <- sprintf('/repos/%s/statuses/%s', repo, sha)
   context <- sprintf('r-universe/%s/%s/deploy', universe, basename(repo))
