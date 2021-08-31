eda708a7d84de1028f85cf0b66bdcc7423bb5f15
Jeroen Ooms <jeroenooms@gmail.com>
2021-08-31 20:22:58 +0200

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 echo "Trying to set commit status for ${1}/commit/${2}"
 echo "Source status: ${6}"
 echo "Docs status: ${7}"
-Rscript -e "commitstatus::gh_app_set_commit_status('${1}','${2}','${3}','${4}','${5}', '${6}', ${7}')"
+Rscript -e "commitstatus::gh_app_set_commit_status('${1}','${2}','${3}','${4}','${5}', '${6}', '${7}')"
 echo "Action complete!"
