ea0939f4e0db54027bd815dc273d9d52147ad392
Jeroen Ooms <jeroenooms@gmail.com>
2023-11-01 22:58:23 +0100

    Refactor action to use named variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 set -e
-echo "Trying to set commit status for ${1}/commit/${2}"
-echo "Source status: ${6}"
-echo "Docs status: ${7}"
-Rscript -e "commitstatus::gh_app_set_commit_status('${1}','${2}','${3}','${4}','${5}', '${6}', '${7}', '${8}')"
+echo "Trying to set commit status for ${REPO}/commit/${SHA}"
+echo "Source status: ${SOURCE_STATUS}"
+echo "Docs status: ${DOCS_STATUS}"
+Rscript -e "commitstatus::gh_app_set_commit_status('${REPO}','${SHA}','${BUILDLOG}','${UNIVERSE}','${DEPLOYED_PACKAGES}', '${SOURCE_STATUS}', '${DOCS_STATUS}', '${OSTYPE}')"
 echo "Action complete!"
