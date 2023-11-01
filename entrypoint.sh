755cee7a7cb80f17d8a12adc559eab9a4fd6e585
Jeroen Ooms <jeroenooms@gmail.com>
2023-11-01 23:25:16 +0100

    Run action unconditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@ set -e
 echo "Trying to set commit status for ${REPO}/commit/${SHA}"
 echo "Source status: ${SOURCE_STATUS}"
 echo "Docs status: ${DOCS_STATUS}"
+if [ "${HAS_APP}" ]; then
 Rscript -e "commitstatus::gh_app_set_commit_status('${REPO}','${SHA}','${BUILDLOG}','${UNIVERSE}','${DEPLOYED_PACKAGES}', '${SOURCE_STATUS}', '${DOCS_STATUS}', '${OSTYPE}')"
+fi
 echo "Action complete!"
