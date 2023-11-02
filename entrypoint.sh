18ab53f28c4dfd2287bce7647e3f085af31bf718
Jeroen Ooms <jeroenooms@gmail.com>
2023-11-02 14:23:47 +0100

    Do not create deployments for rebuilds

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Docs status: ${DOCS_STATUS}"
 Rscript -e "commitstatus::gh_app_set_commit_status('${REPO}','${SHA}','${BUILDLOG}','${UNIVERSE}','${DEPLOYED_PACKAGES}', '${SOURCE_STATUS}', '${DOCS_STATUS}', '${OSTYPE}')"
 fi
 
-if [ "${DEPLOYED_PACKAGES}" != "pending" ]; then
+if [ "${DEPLOYED_PACKAGES}" != "pending" ] && [ -z "${IS_REBUILD}" ]; then
 echo "Create deployment for ${PACKAGE} in https://github.com/r-universe/${UNIVERSE}"
 Rscript -e "commitstatus::update_deployment_status('${UNIVERSE}', '${PACKAGE}', '${GITHUB_SHA}','${BUILDLOG}', '${SOURCE_STATUS}')"
 fi
